548b7d0c1063fa09fc5c4054a2a80891e7bde296
iftikharVTB <engr.iftikhar100@gmail.com>
2019-07-15 03:38:05 +0400

    Integration tests complete

--- src/test/java/com/revolut/transfers/integration/TransferRestResourceTest.java
+++ src/test/java/com/revolut/transfers/integration/TransferRestResourceTest.java
@@ -5,7 +5,6 @@ import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 
 import java.io.IOException;
@@ -27,7 +26,7 @@ public class TransferRestResourceTest {
     @Test
     @DisplayName("should pass when valid available accountId is passed")
     public void testGetAccountByIdValidAvailableAccountIdRequest() {
-        if(checkApplicationRunning()) {
+        if (checkApplicationRunning()) {
             Map<String, Object> response = given()
                     .basePath("transactions/accounts/")
 
@@ -48,7 +47,7 @@ public class TransferRestResourceTest {
     @DisplayName("should return 400 when in valid  accountId is passed")
     public void testGetAccountByIdByInvalidAccountId() {
 
-        if(checkApplicationRunning()) {
+        if (checkApplicationRunning()) {
             given().basePath("transactions/accounts/").
                     when().get("/{accountId}", "ssss")
                     .then().statusCode(400);
@@ -59,7 +58,7 @@ public class TransferRestResourceTest {
     @Test
     @DisplayName("should return 404 when valid but un available accountId is passed")
     public void testGetAccountByIdByValidAccountId() {
-        if(checkApplicationRunning()) {
+        if (checkApplicationRunning()) {
 
             given().basePath("transactions/accounts/").
                     when().get("/{accountId}", "13234").
@@ -70,18 +69,19 @@ public class TransferRestResourceTest {
     @Test
     @DisplayName("should transfer  when requested with valid accountIds and amount")
     public void transferAmountWithValidAccountsAndAmount() {
-        if(checkApplicationRunning()) {
+        if (checkApplicationRunning()) {
             String requestBody = "{\"amount\":20.00,\"currencyCode\":\"USD\"}";
             JsonPath response = given().basePath("transactions/accounts/").
                     pathParam("sourceAccountId", 70001).pathParam("targetAccountId", 70002)
                     .body(requestBody)
-                    .when().put("/{sourceAccountId}/{targetAccountId}")
+                    .when().put("/{sourceAccountId}/{targetAccountId}/transfers")
                     .then().statusCode(200).extract().jsonPath();
             String message = response.getString("message");
             String result = response.get("result");
             assertEquals(message, "Balance Transferred Successfully");
             assertEquals(result, "SUCCESS");
-        } }
+        }
+    }
 
     @Test
     @DisplayName("transfer should fail when requested with invalid accountIds and amount")
@@ -91,7 +91,7 @@ public class TransferRestResourceTest {
             JsonPath response = given().basePath("transactions/accounts/").
                     pathParam("sourceAccountId", 123).pathParam("targetAccountId", 70002)
                     .body(requestBody)
-                    .when().put("/{sourceAccountId}/{targetAccountId}")
+                    .when().put("/{sourceAccountId}/{targetAccountId}/transfers")
                     .then().statusCode(404).extract().jsonPath();
             String message = response.getString("message");
             String result = response.get("result");
@@ -99,28 +99,29 @@ public class TransferRestResourceTest {
             assertEquals(result, "Failure");
         }
     }
+
     @Test
     @DisplayName("transfer should fail when requested with valid accountIds and Invalid amount")
     public void transferAmountWithValidAccountsAndInAmount() {
-        if(checkApplicationRunning()) {
+        if (checkApplicationRunning()) {
             String requestBody = "{\"amount\":-20.00,\"currencyCode\":\"USD\"}";
             JsonPath response = given().basePath("transactions/accounts/").
                     pathParam("sourceAccountId", 70001).pathParam("targetAccountId", 70002)
                     .body(requestBody)
-                    .when().put("/{sourceAccountId}/{targetAccountId}")
+                    .when().put("/{sourceAccountId}/{targetAccountId}/transfers")
                     .then().statusCode(400).extract().jsonPath();
             String message = response.getString("message");
             String result = response.get("result");
             assertEquals(message, "Invalid amount should be greater than 0");
             assertEquals(result, "Failure");
-        }}
-
+        }
+    }
 
 
     @Test
     @DisplayName("withdrawal should be successful when requested with valid accountIds and  amount")
     public void withdrawalAmountWithValidAccountsAndAmount() {
-        if(checkApplicationRunning()) {
+        if (checkApplicationRunning()) {
             String requestBody = "{\"amount\":20.00,\"currencyCode\":\"USD\"}";
             JsonPath response = given().basePath("transactions/accounts/").
                     pathParam("sourceAccountId", 70001)
@@ -129,7 +130,7 @@ public class TransferRestResourceTest {
                     .then().statusCode(200).extract().jsonPath();
             String message = response.getString("message");
             String result = response.get("result");
-            assertEquals( "Withdrawal Successful",message);
+            assertEquals("Withdrawal Successful", message);
             assertEquals(result, "SUCCESS");
         }
     }
@@ -147,98 +148,105 @@ public class TransferRestResourceTest {
                     .then().statusCode(404).extract().jsonPath();
             String message = response.getString("message");
             String result = response.get("result");
-            assertEquals("Account not found for accountId: 123", message);
-            assertEquals("Failure",result);
+            assertEquals("Account not found for accountId: 0", message);
+            assertEquals("Failure", result);
         }
 
     }
-        @Test
-        @DisplayName("Deposit should be successful when requested with valid accountIds")
-        public void depositAmountWithValidAccount() {
-            if (checkApplicationRunning()) {
-                String requestBody = "{\"amount\":20.00,\"currencyCode\":\"USD\"}";
-                JsonPath response = given().basePath("transactions/accounts/").
-                        pathParam("sourceAccountId", 70001)
-                        .body(requestBody)
-                        .when().put("/{sourceAccountId}/deposits")
-                        .then().statusCode(200).extract().jsonPath();
-                String message = response.getString("message");
-                String result = response.get("result");
-                assertEquals("Deposit successful", message);
-                assertEquals("SUCCESS", result);
-            }
 
+    @Test
+    @DisplayName("Deposit should be successful when requested with valid accountIds")
+    public void depositAmountWithValidAccount() {
+        if (checkApplicationRunning()) {
+            String requestBody = "{\"amount\":20.00,\"currencyCode\":\"USD\"}";
+            JsonPath response = given().basePath("transactions/accounts/").
+                    pathParam("sourceAccountId", 70001)
+                    .body(requestBody)
+                    .when().put("/{sourceAccountId}/deposits")
+                    .then().statusCode(200).extract().jsonPath();
+            String message = response.getString("message");
+            String result = response.get("result");
+            assertEquals("Deposit successful", message);
+            assertEquals("SUCCESS", result);
         }
 
+    }
 
-            @Test
-            @DisplayName("Deposit should fail when requested with invalid amount")
-            public void depositAmountWithInValidAmount() {
-                if (checkApplicationRunning()) {
-                    String requestBody = "{\"amount\":-20.00,\"currencyCode\":\"USD\"}";
-                    JsonPath response = given().basePath("transactions/accounts/").
-                            pathParam("sourceAccountId", 70001)
-                            .body(requestBody)
-                            .when().put("/{sourceAccountId}/deposits")
-                            .then().statusCode(400).extract().jsonPath();
-                    String message = response.getString("message");
-                    String result = response.get("result");
-                    assertEquals("Invalid amount should be greater than 0", message);
-                    assertEquals("Failure", result);
-                }
-            }
 
-                @Test
-                @DisplayName("Deposit should fail when requested with invalid currency code")
-                public void depositAmountWithInValidCurrencyCode() {
-                    if(checkApplicationRunning()) {
-                        String requestBody = "{\"amount\":20.00,\"currencyCode\":\"abca\"}";
-                        JsonPath response = given().basePath("transactions/accounts/").
-                                pathParam("sourceAccountId", 70001)
-                                .body(requestBody)
-                                .when().put("/{sourceAccountId}/deposits")
-                                .then().statusCode(400).extract().jsonPath();
-                        String message = response.getString("message");
-                        String result = response.get("result");
-                        assertEquals( "Invalid currency code",message);
-                        assertEquals("Failure", result);
-                    }
+    @Test
+    @DisplayName("Deposit should fail when requested with invalid amount")
+    public void depositAmountWithInValidAmount() {
+        if (checkApplicationRunning()) {
+            String requestBody = "{\"amount\":-20.00,\"currencyCode\":\"USD\"}";
+            JsonPath response = given().basePath("transactions/accounts/").
+                    pathParam("sourceAccountId", 70001)
+                    .body(requestBody)
+                    .when().put("/{sourceAccountId}/deposits")
+                    .then().statusCode(400).extract().jsonPath();
+            String message = response.getString("message");
+            String result = response.get("result");
+            assertEquals("Invalid amount should be greater than 0", message);
+            assertEquals("Failure", result);
+        }
+    }
 
+    @Test
+    @DisplayName("Deposit should fail when requested with invalid currency code")
+    public void depositAmountWithInValidCurrencyCode() {
+        if (checkApplicationRunning()) {
+            String requestBody = "{\"amount\":20.00,\"currencyCode\":\"abca\"}";
+            JsonPath response = given().basePath("transactions/accounts/").
+                    pathParam("sourceAccountId", 70001)
+                    .body(requestBody)
+                    .when().put("/{sourceAccountId}/deposits")
+                    .then().statusCode(400).extract().jsonPath();
+            String message = response.getString("message");
+            String result = response.get("result");
+            assertEquals("Unknown currency code: abca", message);
+            assertEquals("Failure", result);
+        }
+    }
+
+    @Test
+    @DisplayName("withdrawal should fail when requested with bigger amount than available amount in account")
+    public void withdrawalWillFailWhenRequestWithBiggerAmountThanAvailableAmount() {
+        if (checkApplicationRunning()) {
+            String requestBody = "{\"amount\":2200.00,\"currencyCode\":\"USD\"}";
+            JsonPath response = given().basePath("transactions/accounts/").
+                    pathParam("sourceAccountId", 70001)
+                    .body(requestBody)
+                    .when().put("/{sourceAccountId}/withdrawals")
+                    .then().statusCode(400).extract().jsonPath();
+            String message = response.getString("message");
+            String result = response.get("result");
+            assertEquals("Not enough funds for withdrawal", message);
+            assertEquals("Failure", result);
+        }
 
 
     }
 
 
+    /**
+     * Utility method to check of application is running on localhost
+     * the integration  test will be skipped if the application is not available to run integration tests
+     * Some time due to incompatibility between different version of maven and junit 5  surefire plugin ExecutionCondition
+     * do not work for this reason to make sure that build should be successful in any case code of the integration test
+     * only run if this method returns true.
+     * method simply ping at localhost:8089 to check application is running
+     *
+     * @see com.revolut.transfers.anotations.EnabledIfReachableCondition
+     */
+    private boolean checkApplicationRunning() {
 
+        try (Socket socket = new Socket()) {
+            socket.connect(new InetSocketAddress("127.0.0.1", 8089), 2000);
+            return true;
+        } catch (IOException e) {
+            return false; // Either timeout or unreachable or failed DNS lookup.
+        }
 
-
-
-
-
-
-
-
-
-  /**Utility method to check of application is running on localhost
-     the test will ge skip if the application is not available to run integration tests
-     Some time due to incompatibility between different version of maven and junit 5  surefire plugin ExecutionCondition
-     do not work for this reason to make sure that build should be successful in any case code of the integration test
-     only run if this method returns true.
-     method simply ping at localhost:8089 to check application is running
-     @see com.revolut.transfers.anotations.EnabledIfReachableCondition
-    */
-  private boolean checkApplicationRunning(){
-
-      try (Socket socket = new Socket()) {
-          socket.connect(new InetSocketAddress("127.0.0.1", 8089), 5000);
-          return true;
-      } catch (IOException e) {
-          return false; // Either timeout or unreachable or failed DNS lookup.
-      }
-
-  }
-
-
+    }
 
 
 }
